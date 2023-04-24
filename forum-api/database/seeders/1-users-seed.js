c983e3e3aa83cc49a246e7f614bb59cc07a97140
Elber Lopes <elbercaio@gmail.com>
2023-04-24 13:28:45 -0300

    formating

--- forum-api/database/seeders/1-users-seed.js
+++ forum-api/database/seeders/1-users-seed.js
@@ -8,11 +8,9 @@ module.exports = {
       {
         name: 'Roberto',
         email: 'robertinho_totoso@gmail.com',
-        password:
-          '$2b$10$Ouq0nnxtmTfB9KgbQxr8Ge0PKBcFrdFoSa3YMOB/Mz22ogkvEgh7u',
+        password: '$2b$10$Ouq0nnxtmTfB9KgbQxr8Ge0PKBcFrdFoSa3YMOB/Mz22ogkvEgh7u',
       },
     ]),
 
-  down: (queryInterface, _Sequelize) =>
-    queryInterface.bulkDelete('users', null, {}),
+  down: (queryInterface, _Sequelize) => queryInterface.bulkDelete('users', null, {}),
 };
