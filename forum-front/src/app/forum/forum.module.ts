9a2b5be3e99cd464cff770ec63b50fb5ecb9f82c
robertobeltraoo <robertobbentes@gmail.com>
2023-04-24 10:08:54 -0300

    first commit

---
+++ forum-front/src/app/forum/forum.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ForumComponent } from './forum.component';
+
+
+
+@NgModule({
+  declarations: [
+    ForumComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ForumModule { }
