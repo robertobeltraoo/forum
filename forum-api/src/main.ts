76386d64572278e4633de543907f09e4199bc6b5
Elber Lopes <elbercaio@gmail.com>
2023-04-26 10:48:36 -0300

    installing helmet and ratelimit

--- forum-api/src/main.ts
+++ forum-api/src/main.ts
@@ -1,5 +1,9 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
+import * as express from 'express';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 try {
@@ -10,6 +14,23 @@ try {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const globalPrefix = 'forum-api';
+  const port = Number(process.env.PORT) || 3000;
+
+  app.enableCors();
+  app.setGlobalPrefix(globalPrefix);
+  app.use(express.json({ limit: '20mb' }));
+  app.use(express.urlencoded({ limit: '20mb', extended: true, parameterLimit: 1000 }));
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 1 * 60 * 1000,
+      max: 500,
+      message: 'Muitas tentativas, tente novamente mais tarde.',
+    })
+  );
+  await app.listen(port, () => {
+    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  });
 }
 bootstrap();
